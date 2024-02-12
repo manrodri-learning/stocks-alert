b4a2b749d66262c4fdb9a17ca069fc57cdcdf014
manrodri <32561150+manrodri@users.noreply.github.com>
2024-02-12 15:43:15 +0000

    try basic yf library

--- stocks-alert-lambda/handler.py
+++ stocks-alert-lambda/handler.py
@@ -1,10 +1,18 @@
 import json
+import yfinance as yf
+
+
+
 
 
 def hello(event, context):
+    msft = yf.Ticker("MSFT")
+
+    # get all stock info
+    # msft.info
     body = {
         "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
+        "input": msft.info,
     }
 
     return {"statusCode": 200, "body": json.dumps(body)}
